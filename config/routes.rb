4ef61a325201401e778b1c2aafe3cae1dcf1854c
thracken <jeffgreenland.1@gmail.com>
2016-07-07 22:47:17 -0700

    Added bootstrap, users controller, and set up basic home page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
   root 'users#index'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
