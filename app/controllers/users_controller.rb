0f26a84a6b2dafaac578ead613d758118ed4e849
thracken <jeffgreenland.1@gmail.com>
2016-07-08 00:26:05 -0700

    Added Sign Up form for Users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,22 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:sucess] = "Welcome, member!"
+      redirect_to root
+    else
+      flash.now[:error] = "Oops, something's not quite right."
+      render 'new'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name,:email,:password,:password_confirmation)
+    end
 end
