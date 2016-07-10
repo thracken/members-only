2044ce2aa2bad1eac2d1995d20b96541c26bbfcc
thracken <jeffgreenland.1@gmail.com>
2016-07-09 22:29:54 -0700

    Added new form for Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,12 +2,14 @@ class PostsController < ApplicationController
   before_action :check_logged_in, only: [:new, :create]
 
   def new
+    @post = Post.new
   end
 
   def create
   end
 
   def index
+    @posts = Post.all
   end
 
   private
